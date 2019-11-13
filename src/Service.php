8112978dd53290af1a73671a49417b3d43fc825d
Sven Wagener <sven@awesome.ug>
2019-11-13 14:05:05 +0100

    Added service class

---
+++ src/Service.php
@@ -0,0 +1,15 @@
+<?php
+namespace Awsm\WP_Plugin;
+
+/**
+ * Service example
+ */
+class Service {
+    /**
+     * Register hooks
+     *
+     * @return void
+     **/
+    public function register(): void {
+    }
+}
