2aa78beb4ae02ca2cab00c70d3839d6f1b98f4a5
Sven Wagener <sven@awesome.ug>
2019-11-13 14:30:44 +0100

    Added exception

---
+++ src/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Exception wrapper.
+ *
+ * @since 1.0.0
+ */
+namespace Awsmug\WP_Plugin\Exceptions;
+
+/**
+ * Class EnonException.
+ *
+ * @since 1.0.0
+ *
+ * @package Enon\Exceptions
+ */
+class Exception extends \Exception {
+
+}
