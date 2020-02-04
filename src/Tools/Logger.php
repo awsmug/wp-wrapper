4e38dfe2da48fafc00427fcabf122ab1309e2578
Sven Wagener <sven@awesome.ug>
2020-02-04 16:10:48 +0100

    Added logger functionality

--- src/Tools/Logger.php
+++ src/Tools/Logger.php
@@ -11,6 +11,10 @@
 
 namespace Awsm\WP_Wrapper\Tools;
 
+use DateTimeZone;
+use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\StreamHandler;
+
 /**
  * Logger Wrapper.
  *
@@ -19,15 +23,50 @@ namespace Awsm\WP_Wrapper\Tools;
 class Logger extends \Monolog\Logger {
 
 	/**
-	 * Logging error
+	 * Constructor.
 	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $message Message to log and display.
-	 * @param array  $context The log context.
+	 * @param string             $name       The logging channel, a simple descriptive name that is attached to all log records
+	 * @param HandlerInterface[] $handlers   Optional stack of handlers, the first one in the array is called first, etc.
+	 * @param callable[]         $processors Optional array of processors
+	 * @param DateTimeZone|null  $timezone   Optional timezone, if not provided date_default_timezone_get() will be used
 	 */
-	public function error( $message, array $context = array() ): void {
-		parent::error( $message, $context );
-		wp_die( esc_attr( $message ) );
+	public function __construct( string $name, array $handlers = [], array $processors = [], DateTimeZone $timezone = null ) {
+		parent::__construct($name, $handlers, $processors, $timezone);
+
+		$file = $this->get_logging_path() . '/' . $this->get_logging_filename();
+		$this->pushHandler( new StreamHandler( $file, $this->get_debug_level() ) );
+	}
+
+	/**
+	 * Get logging path.
+	 *
+	 * Should be overwritten by child classes to change logging path.
+	 *
+	 * @return string Logging path.
+	 */
+	protected function get_logging_path() {
+		return dirname( ABSPATH );
+	}
+
+	/**
+	 * Get logging filename.
+	 *
+	 * Should be overwritten by child classes to change logging filename.
+	 *
+	 * @return string Logging filename.
+	 */
+	protected function get_logging_filename() {
+		return $this->getName() . '.log';
+	}
+
+	/**
+	 * Get debug level.
+	 *
+	 * Should be overwritten by child classes to change debug level.
+	 *
+	 * @return string Logging filename.
+	 */
+	protected function get_debug_level() {
+		return Logger::DEBUG;
 	}
 }
